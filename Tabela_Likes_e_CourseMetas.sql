85e1352944c62e3151117032661c5005ee4bdf3e
Daniel-Roch <daniel_roch4@hotmail.com>
2021-06-21 16:23:24 -0300

    Mudando o order by para o nome da procura em vez do count

--- Tabela_Likes_e_CourseMetas.sql
+++ Tabela_Likes_e_CourseMetas.sql
@@ -2,14 +2,14 @@
 SELECT A.id `ID_Jogador`,A.firstClear `Nome_Jogador`, Sum(A.stars) `Total_de_Estrelas`
 FROM t5grupo4.`course-meta` A
 group by A.firstClear,A.id
-order by Sum(A.stars) desc
+order by `Total_de_Estrelas` desc
 limit 10;
 
 -- Os 10 primeiros jogadores com maiores Tentativas!
 SELECT A.id `ID_Jogador`,A.firstClear `Nome_Jogador`, Sum(A.attempts) `Total_de_Tentativas`
 FROM t5grupo4.`course-meta` A
 group by A.firstClear,A.id
-order by Sum(A.stars) desc
+order by `Total_de_Tentativas` desc
 limit 10;
 
 -- Os 10 jogadores (Todos os países) que receberam maior quantidade de curtidas;
@@ -17,7 +17,7 @@ SELECT B.id `Nome_Jogador`,B.image `Imagem_Jogador`, count(A.id) `QTA_de_Likes_r
 INNER JOIN t5grupo4.player2 B 
 ON A.player = B.name
 group by A.player,B.id
-order by count(A.id) desc
+order by `QTA_de_Likes_recebido` desc
 limit 10;
 
 -- Os 10 jogadores Brasileiros que receberam maior quantidade de curtidas;
@@ -26,7 +26,7 @@ INNER JOIN t5grupo4.player2 B
 ON A.player = B.name
 group by A.player,B.id
 having B.flag = 'BR'
-order by count(A.id) desc
+order by `QTA_de_Likes_recebido` desc
 limit 10;
 
 -- Fim
