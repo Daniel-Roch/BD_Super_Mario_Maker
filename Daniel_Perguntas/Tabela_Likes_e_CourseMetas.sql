0b462c38d4a6e79724933d5587d43ef423a1af1e
Daniel-Roch <daniel_roch4@hotmail.com>
2021-06-24 15:13:08 -0300

    Colocando 1 comando a mais, para pegar o top 10 de records

--- Daniel_Perguntas/Tabela_Likes_e_CourseMetas.sql
+++ Daniel_Perguntas/Tabela_Likes_e_CourseMetas.sql
@@ -13,7 +13,7 @@ order by `Total_de_Tentativas` desc
 limit 10;
 
 -- Os 10 jogadores (Todos os países) que receberam maior quantidade de curtidas;
-SELECT B.id `Nome_Jogador`,B.image `Imagem_Jogador`, count(A.id) `QTA_de_Likes_recebido`, B.flag `País` FROM t5grupo4.likes A
+SELECT concat(B.id," - ",B.flag) `Nome_Jogador_e_País`,B.image `Imagem_Jogador`, count(A.id) `QTA_de_Likes_recebido` FROM t5grupo4.likes A
 INNER JOIN t5grupo4.player2 B 
 ON A.player = B.name
 group by A.player,B.id
@@ -29,4 +29,9 @@ having B.flag = 'BR'
 order by `QTA_de_Likes_recebido` desc
 limit 10;
 
+-- Os Top 10 com maior tempo de jogo;
+SELECT A.player `Nome_Jogador`, sum(A.timeRecord) `Total_Minutos` FROM t5grupo4.records A
+group by A.player, A.timeRecord
+order by sum(A.timeRecord) desc
+limit 10;
 -- Fim
